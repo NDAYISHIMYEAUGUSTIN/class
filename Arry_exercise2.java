c9237907bc4534998fc84b139d9d7d812adc98fd
NDAYISHIMYEAUGUSTIN <77852050+NDAYISHIMYEAUGUSTIN@users.noreply.github.com>
2021-01-22 20:08:36 +0200

    Add files via upload

---
+++ Arry_exercise2.java
@@ -0,0 +1,14 @@
+public class Arry_exercise2{
+	public static void main(String[]args){
+
+
+		String alpha[]={"a","b","c","d"};
+
+
+		for (String alphabatic:alpha) {
+
+			System.out.println(alphabatic);
+			
+		}
+	}
+}
---
+++ Arry_multidemision.java
@@ -0,0 +1,19 @@
+public class Arry_multidemision{
+
+public static void main(String[] args)
+{
+	int a[][]={{1,2},{3,4},{6,7}};
+
+for(int rows=0;rows<3;rows++)
+{
+
+for(int cols=0;cols<2;cols++)
+{
+System.out.print(a[rows][cols]);
+}
+System.out.print("\n");
+}
+
+}
+
+}
